e3a510d30613dd4e601c565880af9cd924b57819
Lewis Beischer <l.beischer@lek.com>
2017-10-24 11:44:03 +0100

    Making single exe package

--- xml_logging_app/setup.py
+++ xml_logging_app/setup.py
@@ -1,6 +1,12 @@
 # Setup script for app_messenger
 
 from distutils.core import setup
-import py2exe
+import py2exe, sys, os
 
-setup(console=['xml_logging_enabler.py'])
+sys.argv.append('py2exe')
+
+setup(
+	options = {'py2exe': {'bundle_files': 1}},
+	windows = [{'script': "xml_logging_enabler.py"}],
+	zipfile = None,
+)
